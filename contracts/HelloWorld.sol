f492fcdb2ef47c8343e2710452c00fb0b22d1784
nubro999 <zcb167@gmail.com>
2025-07-15 17:52:55 +0900

    first commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+  string public message = "Hello Truffle!";
+
+  function setMessage(string memory _message) public {
+    message = _message;
+  }
+}
